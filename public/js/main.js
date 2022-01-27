f330ad13ca0ce82535d82959921371ad98f229ed
nishant0811 <renegade.plays03@gmail.com>
2022-01-27 14:39:31 +0530

    random players spawn

--- public/js/main.js
+++ public/js/main.js
@@ -4,14 +4,14 @@ let username = 'nishant'
 let playerId
 socket.emit('joinRoom' , {username})
 
-socket.on('mapSize',(data)=>{
-  document.getElementById('map').innerHTML= ``;
-  for(let i=0; i<data*data ;i++){
-    document.getElementById('map').innerHTML +=`
-    <div class="square">
+let mapSize;
 
-    </div>
-    `
+socket.on('mapSize',(data)=>{
+  document.getElementById('map').innerHTML= "";
+  mapSize = data;
+  for(let i=0; i<data ;i++){
+    for(let j=0;j<data;j++)
+    document.getElementById('map').innerHTML +=`<div class="square" id="${i}${j}"></div>`;
   }
 })
 
@@ -19,4 +19,15 @@ socket.on('mapSize',(data)=>{
 socket.on('setId' , (data)=>{
   playerId = data;
   console.log(playerId);
+})
+
+
+socket.on('gameData' , (data)=>{
+  document.getElementById('map').innerHTML= "";
+  let map = data.map;
+  for(let i=0 ; i<mapSize ; i++){
+    for(let j=0; j<mapSize ; j++){
+      document.getElementById('map').innerHTML +=`<div class="square ${map[i][j].color}" id="${i}${j}"></div>`;
+    }
+  }
 })
--- server.js
+++ server.js
@@ -9,11 +9,61 @@ app.use(express.static("public"))
 const io = socket(server)
 
 
-let users = []
-
+let users = [];
+let color = ['red', 'blue' , 'yellow' , 'green' , 'black' , 'brown' , 'orange'];
+let maxPlayers = 7;
 let mapSize = 20;
 
+let gameMap =[];
+let usersMap = [];
+let gameStats=[];
+
+
+let players = [];
+
+
+let xOcc = [];
+let yOcc = [];
+
+function generatePosition(){
+  let min =0;
+  let max = mapSize-1;
+  let x = Math.floor(Math.random() * (max - min + 1) + min);
+  let y = Math.floor(Math.random() * (max - min + 1) + min);
+  while(xOcc.includes(x)){
+    x = Math.floor(Math.random() * (max - min + 1) + min);
+  }
+  xOcc.push(x);
+  while(yOcc.includes(y)){
+    y = Math.floor(Math.random() * (max - min + 1) + min);
+  }
+  yOcc.push(y);
+
+  return([x,y]);
+}
+
+
+
+function updatePlayerMap(){
+  for(let i=0; i<mapSize ; i++){
+    for(let j=0; j<mapSize ; j++){
+      for(let k=0 ;k<usersMap.length ; k++){
+        if(usersMap[k].id != gameMap[i][j].owned){
+          usersMap[k].map[i][j].color = gameMap[i][j].color;
+        }
+        else{
+          usersMap[k].map[i][j] = gameMap[i][j];
+        }
+      }
+    }
+  }
+}
+
+
+
 io.on('connection' , socket =>{
+
+
   socket.on('joinRoom' , (data)=>{
     let user = { id : socket.id , username : data.username}
     users.push(user)
@@ -21,6 +71,101 @@ io.on('connection' , socket =>{
     io.to(user.id).emit('setId',user.id)
     console.log(users);
   })
+
+
+
+  socket.on('startGame', ()=>{
+    for(let i=0; i<mapSize ;i++){
+      gameMap.push([]);
+      for(let j=0; j<mapSize ; j++){
+        gameMap[i].push({
+          owned : "",
+          troops : "",
+          goldMine : false,
+          tower : false,
+          fortification : false,
+          color : "",
+        })
+      }
+    }
+    for(let i=0; i<Math.min(maxPlayers , users.length) ; i++){
+      let user = users[i];
+      let position = generatePosition();
+      console.log(position);
+      let userGameData ={
+        id : user.id,
+        map : []
+
+      }
+
+      players.push({
+        id : user.id,
+        username : user.username,
+        color : color[i]
+      })
+      for(let k=0; k<mapSize ;k++){
+        userGameData.map.push([]);
+        for(let j=0; j<mapSize ; j++){
+          userGameData.map[k].push({
+            owned : "",
+            troops : "",
+            goldMine : false,
+            tower : false,
+            fortification : false,
+            color : "",
+          })
+        }
+      }
+
+      userGameData.map[position[0]][position[1]] ={
+        owned : user.id,
+        troops : 500,
+        goldMine : true,
+        tower : true,
+        fortification : true,
+        color : color[i],
+      }
+
+      gameMap[position[0]][position[1]] ={
+        owned : user.id,
+        troops : 500,
+        goldMine : true,
+        tower : true,
+        fortification : true,
+        color : color[i],
+      }
+
+      let stats = {
+        id : user.id,
+        goldMine : 1,
+        territory : 1,
+        energy : 5,
+        gold : 200
+      }
+
+      gameStats.push(stats);
+      usersMap.push(userGameData);
+    }
+
+    updatePlayerMap();
+    for(let i=0; i<Math.min(maxPlayers , usersMap.length);i++){
+      let payload = {
+        map : [],
+        stats : gameStats[i]
+      }
+      let id = gameStats[i].id;
+      usersMap.forEach((user, i) => {
+        if(user.id == id){
+          payload.map = user.map
+        }
+      });
+
+      io.to(id).emit('gameData' , payload);
+    }
+  })
+
+
+
 
   socket.on('disconnect', () =>{
     let playerDisconnected = socket.id
@@ -34,6 +179,9 @@ io.on('connection' , socket =>{
 
     }
   })
+
+
+
 })
 
 
